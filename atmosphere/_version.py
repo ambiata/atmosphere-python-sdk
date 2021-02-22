145abb99a2ce60e6cd680f984c1088176e5811ea
James Karefylakis <jamylak@gmail.com>
2021-02-22 14:44:59 +1100

    Fix for version import

--- atmosphere/_version.py
+++ atmosphere/_version.py
@@ -1,5 +1,9 @@
+import pathlib
+
+current_dir = pathlib.Path(__file__).parent.absolute()
+
 def get_version():
-    with open('VERSION') as version_file:
+    with open(current_dir / "VERSION") as version_file:
         return version_file.readlines()[1].strip()
 
 
