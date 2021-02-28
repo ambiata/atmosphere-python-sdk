aa3b082871771496def8af1fb63ad20b53bc192b
James Karefylakis <jamylak@gmail.com>
2021-03-01 09:45:11 +1100

    Lint 2 places in lint script

--- atmosphere/scripts/lint.sh
+++ atmosphere/scripts/lint.sh
@@ -4,6 +4,7 @@ set -x
 EXIT_STATUS=0
 
 isort --recursive --check-only app || EXIT_STATUS=$?
-pylint app || EXIT_STATUS=$?
+pylint activity || EXIT_STATUS=$?
+pylint transformer || EXIT_STATUS=$?
 
 exit $EXIT_STATUS
