b862e66efc0e42b4daa613fddf1dce10ed46af18
Guillaume Smith <guillaume.smith@ambiata.com>
2021-03-12 17:01:33 +1100

    Change python requires from 3.6 to 3.6.0.

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-import setuptools
 import pathlib
 
+import setuptools
+
 current_dir = pathlib.Path(__file__).parent.absolute()
+
 
 def get_version():
     with open(current_dir / "atmosphere" / "VERSION") as version_file:
         return version_file.readlines()[1].strip()
 
+
 setuptools.setup(
     name="atmosphere",
     author="Ambiata",
-    description="The atmosphere python SDK including multiple modules to help with setting up activities, transformers and more.",
+    description="The atmosphere python SDK including multiple modules to help with setting up activities, "
+                "transformers and more.",
     long_description_content_type="text/markdown",
     url="https://github.com/ambiata/atmosphere-python-sdk.git",
     packages=setuptools.find_packages(),
     include_package_data=True,
     version=get_version(),
-    python_requires='>=3.6',
+    python_requires='>=3.6.0',
     install_requires=[
         'apscheduler>=3.6.3',
         'fastapi>=0.61.1',
